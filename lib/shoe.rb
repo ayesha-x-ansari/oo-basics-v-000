6a4fee0169662db5d03890ac77cbdce297e14de5
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-11 16:27:12 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material
   attr_reader :brand
- 
+
   def initialize(brand)
     @brand = brand
   end
- 
+
   def cobble
     puts "Your shoe is as good as new!"
   end
- 
+
 end# Make your shoe class here!
